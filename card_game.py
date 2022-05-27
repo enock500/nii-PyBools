6e6024301a8a2f2d59ef2e265147d77b21b08344
your_github_username <your_github_email>
2022-05-27 09:56:33 -0500

    update of game1

--- card_game.py
+++ card_game.py
@@ -27,8 +27,8 @@ Otherwise, player2 will start the next round.
 
 # Declare two variables `player1` and `player2` and set them as the
 # names of the players in the game.
-player1 = "james "
-player2 = "jane"
+player1 = "James"
+player2 = "Jane"
 # Declare each of the suits (clubs, diamonds, hearts, and spades) as an int variable and set its value to its appropriate numerical value.
 # (Remember: Each suit is given a numerical value based on the numerical equivalent of the letter it begins with. See the assignment overview
 # for more details on this)
@@ -50,7 +50,6 @@ spades = 19
 # Jane is dealt the following cards: 2 Spades, Queen Spades, 10 Diamonds, 7 Clubs , Jack Hearts
 
 #Round 1
-game = 3
 
 Jack = 11
 Queen = 12
@@ -62,7 +61,7 @@ Ace = 14
 # For each list, recall that the number on each card is added to the numerical value of its suit to determine how many points it's worth.
 # eg. 5 of hearts will be worth 13 points and so the entry in the list will be 5 + <the numerical value of hearts>.
 #  Note: DO NOT COMPUTE THESE BY HAND. Rather, use the variables you declared above!
-james_cards = [
+James_cards = [
     5 + hearts,
     3 + clubs,
     Ace + clubs,
@@ -70,7 +69,7 @@ james_cards = [
     2 + diamonds,
 ]
 
-jane_cards = [
+Jane_cards = [
     2 + spades,
     Queen + spades,
     10 + diamonds,
@@ -81,105 +80,108 @@ jane_cards = [
 # Based on their strategies, play all 5 rounds of the game, and determine the winner at the end of the game.
 # Set the variable `winner` to the name of the winner (through the appropriate player variable above) at the end of the game,
 # Increment the `game` variable to signify the start of the next game.
-round_1 = (james_cards[0] >= jane_cards[0])
-round_2 = (james_cards[1] >= jane_cards[1])
-round_3 = (james_cards[2] >= jane_cards[2])
-round_4 = (james_cards[3] >= jane_cards[3])
-round_5 = (james_cards[4] >= jane_cards[4])
-
+game = 1
+round_1 = (James_cards[0] >= Jane_cards[4])
 print(round_1)
+round_2 = (Jane_cards[0] >= James_cards[2])
 print(round_2)
+round_3 = (Jane_cards[1] >= James_cards[4])
 print(round_3)
+round_4 = (Jane_cards[3] >= James_cards[1])
 print(round_4)
+round_5 = (Jane_cards[2] >= James_cards[3])
 print(round_5)
 
-winner = player2
-print(winner, "you won game 1") 
+
+# winner = player2
+# print(winner, "you won game 1") 
 
 
-#Incrementation of game 
-game = game + 1
+# #Incrementation of game 
+# game = game + 1
 
 
 
 
-# For game 2:
-# James is dealt the following cards: 7 Clubs, 5 Spades, 2 Hearts, Jack Clubs, 2 Spades
-# Jane is dealt the following cards: Jack Hearts, King Diamonds, 8 Clubs, 9 Hearts , 7 Spades
+# # For game 2:
+# # James is dealt the following cards: 7 Clubs, 5 Spades, 2 Hearts, Jack Clubs, 2 Spades
+# # Jane is dealt the following cards: Jack Hearts, King Diamonds, 8 Clubs, 9 Hearts , 7 Spades
 
-james_cards = [
-    7 + clubs,
-    5 + spades,
-    2 + hearts,
-    Jack + clubs,
-    2 + spades,
-]
+# James_cards = [
+#     7 + clubs,
+#     5 + spades,
+#     2 + hearts,
+#     Jack + clubs,
+#     2 + spades,
+# ]
 
-jane_cards = [
-    Jack + hearts,
-    King + diamonds,
-    8 + clubs,  
-    9 + hearts, 
-    7 + spades,
-]
+# Jane_cards = [
+#     Jack + hearts,
+#     King + diamonds,
+#     8 + clubs,  
+#     9 + hearts, 
+#     7 + spades,
+# ]
 
-# Follow the same algorithm to determine the winner of this game
-# Don't forget to increment the game variable!
-round_1 = (james_cards[0] >= jane_cards[0])
-round_2 = (james_cards[1] >= jane_cards[1])
-round_3 = (james_cards[2] >= jane_cards[2])
-round_4 = (james_cards[3] >= jane_cards[3])
-round_5 = (james_cards[4] >= jane_cards[4])
+# # Follow the same algorithm to determine the winner of this game
+# # Don't forget to increment the game variable!
+# game = 2
+# round_1 = (James_cards[0] >= Jane_cards[0])
+# round_2 = (James_cards[1] >= Jane_cards[1])
+# round_3 = (James_cards[2] >= Jane_cards[2])
+# round_4 = (James_cards[3] >= Jane_cards[3])
+# round_5 = (James_cards[4] >= Jane_cards[4])
 
-print(round_1)
-print(round_2)
-print(round_3)
-print(round_4)
-print(round_5)
+# print(round_1)
+# print(round_2)
+# print(round_3)
+# print(round_4)
+# print(round_5)
 
-winner = player2
-print(winner, "you won game 2")
+# winner = player2
+# print(winner, "you won game 2")
 
-#Incrementation of game 
-game = game + 1
-# For game 3:
-# James is dealt the following cards: 10 Diamonds, 2 Clubs, 3 Spades, 7 Diamonds, 4 Spades
-# Jane is dealt the following cards: Queen Hearts, 9 Diamonds, 4 Hearts, 2 Diamonds , Queen Hearts
-james_cards = [
-    10 + diamonds,
-    2 + clubs,
-    3 + spades,
-    7 + diamonds,
-    4 + spades,
-]
+# #Incrementation of game 
+# game = game + 1
+# # For game 3:
+# # James is dealt the following cards: 10 Diamonds, 2 Clubs, 3 Spades, 7 Diamonds, 4 Spades
+# # Jane is dealt the following cards: Queen Hearts, 9 Diamonds, 4 Hearts, 2 Diamonds , Queen Hearts
+# James_cards = [
+#     10 + diamonds,
+#     2 + clubs,
+#     3 + spades,
+#     7 + diamonds,
+#     4 + spades,
+# ]
 
-jane_cards = [
-    Queen + hearts,
-    9 + diamonds,
-    4 + hearts,
-    2 + diamonds,
-    Queen + hearts,
-]
+# Jane_cards = [
+#     Queen + hearts,
+#     9 + diamonds,
+#     4 + hearts,
+#     2 + diamonds,
+#     Queen + hearts,
+# ]
 
-# Follow the same algorithm to determine the winner of this game
-# Print the value of the game variable to signify the end of the game
+# # Follow the same algorithm to determine the winner of this game
+# # Print the value of the game variable to signify the end of the game
+# game = 3
 
-round_1 = (james_cards[0] >= jane_cards[0])
-round_2 = (james_cards[1] >= jane_cards[1])
-round_3 = (james_cards[2] >= jane_cards[2])
-round_4 = (james_cards[3] >= jane_cards[3])
-round_5 = (james_cards[4] >= jane_cards[4])
+# round_1 = (James_cards[0] >= Jane_cards[0])
+# round_2 = (James_cards[1] >= Jane_cards[1])
+# round_3 = (James_cards[2] >= Jane_cards[2])
+# round_4 = (James_cards[3] >= Jane_cards[3])
+# round_5 = (James_cards[4] >= Jane_cards[4])
 
 
-print(round_1)
-print(round_2)
-print(round_3)
-print(round_4)
-print(round_5)
+# print(round_1)
+# print(round_2)
+# print(round_3)
+# print(round_4)
+# print(round_5)
 
-winner = player1
-print(winner, "you won game 3")  
-# Declare a variable `overall_winner` and set it equal to the player who won the most out of all 3 games
-# Set this variable through the player number variable. Print the value of this variables.....
-overall_winner = player2
-print(overall_winner, "declared winner")
+# winner = player1
+# print(winner, "you won game 3")  
+# # Declare a variable `overall_winner` and set it equal to the player who won the most out of all 3 games
+# # Set this variable through the player number variable. Print the value of this variables.....
+# overall_winner = player2
+# print(overall_winner, "declared winner")
